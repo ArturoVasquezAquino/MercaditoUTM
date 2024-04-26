b56c8deea762557176d713fdf38ef1d1776bba9d
DavidMatias44 <daosmaru3667@gmail.com>
2024-04-25 20:54:54 -0600

    last changes from ArthurBranch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.firebase.storage.ktx)
+    implementation(libs.firebase.database.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
